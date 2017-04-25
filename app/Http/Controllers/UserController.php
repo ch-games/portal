76a87b2439e4a0d78261df06ba8a0fdb61f261af
ChenXinGuang1003 <583860027@qq.com>
2017-04-25 11:37:21 +0800

    去除各种页面错误

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Request;
 class UserController extends Controller
 {
     //
     public function index($type)
     {
-        return view("user.{$type}");
+        return;
     }
 
 }
